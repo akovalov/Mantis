73628811b9791cc6b95d80efe6c43390703a599a
Alex Kovalov <alex.kovalov@requestum.com>
2021-11-21 21:17:29 +0200

    Chnage visual effect color from black to clear

--- Sources/Mantis/MaskBackground/CropVisualEffectView.swift
+++ Sources/Mantis/MaskBackground/CropVisualEffectView.swift
@@ -47,7 +47,7 @@ class CropVisualEffectView: UIVisualEffectView, CropMaskProtocol {
             case .blurDark: return (UIBlurEffect(style: .dark), .clear)
             case .dark: return (nil, UIColor.black.withAlphaComponent(0.75))
             case .light: return (nil, UIColor.black.withAlphaComponent(0.35))
-            case .none: return (nil, .black)
+            case .none: return (nil, .clear)
         }
     }
     
